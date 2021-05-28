eb6732b39e1dfa8add662cb8f5febd7d680842a8
Javinator9889 <dev@javinator9889.com>
2021-05-28 16:30:42 +0200

    initial

--- app/src/main/java/com/javinator9889/greenplaces/views/MainActivity.kt
+++ app/src/main/java/com/javinator9889/greenplaces/views/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.javinator9889.greenplaces
+package com.javinator9889.greenplaces.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.javinator9889.greenplaces.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        with(Intent(this, FirebaseUIActivity::class.java)) {
+            startActivity(this)
+        }
     }
 }
